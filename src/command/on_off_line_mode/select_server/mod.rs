eca989d39aa4f4b1877ef38921df683452553a2c
FroVolod <frol_off@meta.ua>
2021-02-06 20:04:39 +0200

    refactoring

---
+++ src/command/on_off_line_mode/select_server/mod.rs
@@ -0,0 +1,102 @@
+use structopt::StructOpt;
+use std::str::FromStr;
+use strum_macros::{
+    Display,
+    EnumString,
+    EnumVariantNames,
+};
+use strum::VariantNames;
+use dialoguer::{
+    Select,
+    Input,
+    theme::ColorfulTheme,
+    console::Term
+};
+
+use crate::consts;
+use consts::{
+    TESTNET_API_SERVER_URL,
+    MAINNET_API_SERVER_URL,
+    BETANET_API_SERVER_URL,
+};
+mod server;
+use server::{
+    Server,
+    SendFrom,
+    CliServer,
+    CliCustomServer,
+    // ActionSubcommand
+};
+
+#[derive(Debug, Display, EnumVariantNames, StructOpt)]
+pub enum SelectServer {
+    Testnet(Server),
+    Mainnet(Server),
+    Betanet(Server),
+    Custom(Server),
+}
+
+#[derive(Debug, Display, EnumVariantNames, StructOpt)]
+pub enum CliSelectServer {
+    Testnet(CliServer),
+    Mainnet(CliServer),
+    Betanet(CliServer),
+    Custom(CliCustomServer),
+}
+
+impl From<CliSelectServer> for SelectServer {
+    fn from(item: CliSelectServer) -> Self {
+        match item {
+            CliSelectServer::Testnet(cli_server) => {
+                Self::Testnet(cli_server.into_server(TESTNET_API_SERVER_URL.to_string()))
+            },
+            CliSelectServer::Mainnet(cli_server) => {
+                Self::Mainnet(cli_server.into_server(MAINNET_API_SERVER_URL.to_string()))
+            },
+            CliSelectServer::Betanet(cli_server) => {
+                Self::Betanet(cli_server.into_server(BETANET_API_SERVER_URL.to_string()))
+            },
+            CliSelectServer::Custom(cli_custom_server) => {
+                Self::Custom(cli_custom_server.into_server())
+            },
+        }
+    }
+}
+
+impl SelectServer {
+    pub fn select_server() -> Self {
+        println!("Works select server!");
+        let servers= SelectServer::VARIANTS;
+        let select_server = Select::with_theme(&ColorfulTheme::default())
+            .with_prompt("Select NEAR protocol RPC server:")
+            .items(&servers)
+            .default(0)
+            .interact_on_opt(&Term::stderr())
+            .unwrap();
+        let send_from = SendFrom::send_from();
+        match select_server {
+            Some(0) => SelectServer::Testnet(Server{
+                            url: TESTNET_API_SERVER_URL.to_string(),
+                            send_from
+                        }),
+            Some(1) => SelectServer::Mainnet(Server{
+                            url: MAINNET_API_SERVER_URL.to_string(),
+                            send_from
+                        }),
+            Some(2) => SelectServer::Betanet(Server{
+                            url: BETANET_API_SERVER_URL.to_string(),
+                            send_from
+                        }),
+            Some(4) => SelectServer::Custom(Server{
+                            url: {
+                                Input::new()
+                                .with_prompt("What is the RPC endpoint?")
+                                .interact_text()
+                                .unwrap()
+                            },
+                            send_from
+            }),
+            _ => unreachable!("Error")
+        }
+    }
+}
